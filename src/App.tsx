1f026ccb54c5beda742966335272f08160fb32d8
Ujin28 <melon4092@naver.com>
2023-08-19 18:33:32 +0900

    calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import AddDiary from './addDiary';
+import Calendar from "./calendar";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<AddDiary />} />
+          <Route path="/calendar" element={<Calendar />} />
       </Routes>
   );
 }
